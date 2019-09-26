28b0ff8ff0533158fd08673005e837d8e3117208
Norbert de Langen <ndelangen@me.com>
2019-09-26 17:52:40 +0200

    CLEANUP

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,6 @@ const warn = 1;
 const ignore = 0;
 
 module.exports = {
-  root: true,
   extends: [
     'airbnb',
     'plugin:jest/recommended',
@@ -157,41 +156,4 @@ module.exports = {
     '@typescript-eslint/no-use-before-define': ignore, // this is duplicated
     '@typescript-eslint/interface-name-prefix': ignore, // I don't agree
   },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/**',
-        '**/*.test.*',
-        '**/*.stories.*',
-        '**/storyshots/**/stories/**',
-        'docs/src/new-components/lib/StoryLinkWrapper.js',
-        'docs/src/stories/**',
-      ],
-      rules: {
-        'import/no-extraneous-dependencies': ignore,
-      },
-    },
-    { files: '**/.storybook/config.js', rules: { 'global-require': ignore } },
-    {
-      files: ['**/*.stories.*'],
-      rules: {
-        'no-console': ignore,
-      },
-    },
-    {
-      files: ['**/*.tsx', '**/*.ts'],
-      rules: {
-        'react/prop-types': ignore, // we should use types
-        'no-dupe-class-members': ignore, // this is called overloads in typescript
-      },
-    },
-    {
-      files: ['**/*.d.ts'],
-      rules: {
-        'vars-on-top': ignore,
-        'no-var': ignore, // this is how typescript works
-        'spaced-comment': ignore,
-      },
-    },
-  ],
 };
