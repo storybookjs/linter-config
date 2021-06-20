6507c20e24a5561d936758168665217fd80a7a12
IgorSzyporyn <igor.szyporyn@gmail.com>
2021-06-20 13:22:34 +0200

    fix: not confident enough to set ecmaVersion to 2020

--- eslint.config.js
+++ eslint.config.js
@@ -19,7 +19,7 @@ module.exports = {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 2020,
+    ecmaVersion: 8,
     sourceType: 'module',
     extraFileExtensions: ['.html', '.md', '.json', '.svg', '.tag'],
   },
