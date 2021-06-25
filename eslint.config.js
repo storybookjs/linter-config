aa21ae3915f1415c23390a7a8f16e4b8b756e5dd
IgorSzyporyn <igor.szyporyn@gmail.com>
2021-06-25 16:41:56 +0200

    fix: @typescript-eslint/explicit-module-boundary-types set to off

--- eslint.config.js
+++ eslint.config.js
@@ -60,6 +60,7 @@ module.exports = {
     '@typescript-eslint/camelcase': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
